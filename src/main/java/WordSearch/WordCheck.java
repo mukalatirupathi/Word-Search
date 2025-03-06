f6d9fc62648ae6cb1219c3ca3047e321873f78ba
Tirupathi Mukala <tirupathi.mukala@theelixrlabs.com>
2025-03-06 17:44:11 +0530

    first commit

---
+++ src/main/java/WordSearch/WordCheck.java
@@ -0,0 +1,11 @@
+package WordSearch;
+import java.util.Scanner;
+public class WordCheck {
+    public static void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the word");
+        String name=sc.next();
+        System.out.println("you entered  name is " + name);
+    }
+}
