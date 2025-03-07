b7987f49b51025a333ee9924c66b73f01967a0fb
Tirupathi Mukala <tirupathi.mukala@theelixrlabs.com>
2025-03-07 15:37:47 +0530

    second commit

--- src/main/java/WordSearch/WordCheck.java
+++ src/main/java/WordSearch/WordCheck.java
@@ -1,11 +1,49 @@
 package WordSearch;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Scanner;
+
 public class WordCheck {
+
+    public void pathcheck() {
+//         Scanner sc =new Scanner(System.in);
+//        System.out.println("enter the path");
+//        String path=sc.nextLine();
+//        System.out.println("you entered  path is " + path);
+//        System.out.println(" plzz enter the word");
+//        String word=sc.nextLine();
+//        System.out.println("you entered  word is " + word);
+//         File fc=new File(path);
+//         if (fc.exists()) {
+//             System.out.println("processing....");
+//         } else{
+//             System.out.println("your entered  file is not found......");
+//         }
+    }
+
     public static void main(String[] args) {
 
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter the word");
-        String name=sc.next();
-        System.out.println("you entered  name is " + name);
+        WordCheck pc = new WordCheck();
+        pc.pathcheck();
+        String path = args[0];
+        String word = args[1];
+        System.out.println("you entered  path is " + path);
+        System.out.println("you entered  word is " + word);
+        File filecheck = new File(path);
+        if (filecheck.exists()) {
+            System.out.println("processing....");
+        } else {
+            System.out.println(" your entered file is  not found......");
+        }
+//        FileInputStream fis = new FileInputStream("filecheck");
+//         int asciicode;
+//         while((asciicode = fis.read())!=-1){
+//             System.out.print(char(asciicode));
+//         }
+        }
     }
-}
+
+
+
+
+
