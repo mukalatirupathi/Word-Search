bc2dc9bb9f8a658ab2737c49e86965dbc7a3f72f
tirupathi.mukala <tirupathimukala0808@gmail.com>
2025-03-13 11:12:32 +0530

    Resolved all comments

--- src/main/java/wordtrace/WordTraceTool.java
+++ src/main/java/wordtrace/WordTraceTool.java
@@ -1,19 +1,19 @@
 package wordtrace;
 
-import constants.LogArgumentConstants;
-import java.io.File;
+import constants.InputFileConstants;
 
+import java.io.File;
 public class WordTraceTool {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String path = args[0];
         String word = args[1];
-        System.out.println(LogArgumentConstants.EnteredPath + path);
-        System.out.println(LogArgumentConstants.EnteredWord + word);
+        System.out.println(InputFileConstants.EnteredPath + path);
+        System.out.println(InputFileConstants.EnteredWord + word);
         File fileCheck = new File(path);
-        if (fileCheck.exists() && fileCheck.isFile() && (path.endsWith(LogArgumentConstants.TextExtensionMessage) || path.endsWith(LogArgumentConstants.JsonExtensionMessage))) {
-            System.out.println(LogArgumentConstants.TextMessage);
+        if (fileCheck.exists() && fileCheck.isFile() && (path.endsWith(InputFileConstants.TextExtensionMessage) || path.endsWith(InputFileConstants.JsonExtensionMessage))) {
+            System.out.println(InputFileConstants.TextMessage);
         } else {
-            System.out.println(LogArgumentConstants.ErrorMessage);
+            System.out.println(InputFileConstants.ErrorMessage);
         }
     }
 }
