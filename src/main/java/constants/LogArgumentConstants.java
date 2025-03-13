e86c72bb8aebfeea7d8facf2e660a023910d8bbd
tirupathi.mukala <tirupathimukala0808@gmail.com>
2025-03-13 11:00:44 +0530

    resloved all comments and removed word count functionality

--- src/main/java/constants/LogArgumentConstants.java
+++ src/main/java/constants/LogArgumentConstants.java
@@ -1,13 +1,10 @@
 package constants;
 
 public class LogArgumentConstants {
-   public static final String TextMessage ="processing....";
-   public static final String  ErrorMessage ="your entered file is  not found or we accepted only .txt or .json files ......";
-   public static final String TextExtensionMessage=".txt";
-   public static final String JsonExtensionMessage=".json";
    public static final String EnteredPath ="you entered Path is ";
+   public static final String  ErrorMessage ="your entered file is  not found or we accepted only .txt or .json files ......";
    public static final String EnteredWord ="you entered Word is ";
-   public static final String SearchedWord ="your searched word will be appeared in this file is ";
-   public static final String ApperedWord= " times ";
-
+   public static final String JsonExtensionMessage=".json";
+   public static final String TextExtensionMessage=".txt";
+   public static final String TextMessage ="processing....";
 }
